191cfa88186151663a31eb30c72929da510b29cd
ekkrug <37642473+ekkrug@users.noreply.github.com>
2018-11-20 16:24:39 -0800

    Initial Commit

---
+++ SendIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendIt
+//
+//  Created by Eugene Krug on 11/20/18.
+//  Copyright © 2018 SendIt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
