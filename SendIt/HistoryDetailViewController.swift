f3c37e4e522afe2afd7ddc9853d8b503e445d07d
Kevin Mattappally <kevinmat@outlook.com>
2018-12-08 15:32:46 -0800

    details feet/meters swapping working

--- SendIt/HistoryDetailViewController.swift
+++ SendIt/HistoryDetailViewController.swift
@@ -21,24 +21,51 @@ class HistoryDetailViewController: UIViewController
     @IBOutlet var mountainImage: UIImageView!
     
     var run: Run? = nil
-    
+    var isFeet: Bool = true
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        let tabBar = tabBarController as! MyTabBarController
+        isFeet = tabBar.isFeet
+        updateView()
+    }
+    
+    func updateView() {
         guard let validRun = run else {
             return
         }
         mountainRunLabel.text = "\(String(describing: validRun.mountain!)): \(String(describing: validRun.name!))"
-        elevationChangeLabel.text = "Elevation Change: \(validRun.elevationChange) feet"
+        let nF = NumberFormatter()
+        nF.minimumFractionDigits = 2
+        nF.maximumFractionDigits = 2
+        var elevationFinal: String = ""
+        if isFeet {
+            let eCFeetNSNum = NSNumber(value: validRun.elevationChange)
+            if let eCFeetDisp = nF.string(from: eCFeetNSNum) {
+                elevationFinal = "\(eCFeetDisp) feet"
+            } else {
+                elevationFinal = "unknown"
+            }
+        } else {
+            let eCMetersDbl = Conversion.feetToMeters(inFeet: validRun.elevationChange)
+            let eCMetersNSNum = NSNumber(value: eCMetersDbl)
+            if let ecMetersDisp = nF.string(from: eCMetersNSNum)
+            {
+                elevationFinal = "\(ecMetersDisp) meters"
+            }
+            else
+            {
+                elevationFinal = "unknown"
+            }
+        }
+        elevationChangeLabel.text = "Elevation Change: \(elevationFinal)"
         difficultyLabel.text = "Difficulty: \(String(describing: validRun.difficulty!))"
-        // timeToTimeLabel.text = "\(String(describing: validRun.startDateTime!)) - \(String(describing: validRun.endDateTime!))"
         let detailDateFormatter = DateFormatter()
         let detailTimeFormatter = DateFormatter()
         detailTimeFormatter.dateStyle = .none
         detailTimeFormatter.timeStyle = .medium
         detailDateFormatter.dateFormat = "MM/dd/yyyy"
-        
         if let startTime = validRun.startDateTime as Date?, let endTime = validRun.endDateTime as Date? {
             dateLabel.text = "Date: \(detailDateFormatter.string(from: startTime))"
             elapsedTimeLabel.text = "Time Duration: \(detailTimeFormatter.string(from: startTime)) - \(detailTimeFormatter.string(from: endTime))"
